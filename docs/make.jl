0e71f091581a51d1c6be5c99ff7a54cf987ddb5d
Jesse E <jesse.d.everington@gmail.com>
2026-04-01 17:57:28 -0400

    Configure deployment for jlabclouds GitHub Pages

--- docs/make.jl
+++ docs/make.jl
@@ -62,7 +62,7 @@ makedocs(
     # HTML format configuration for GitHub Pages compatibility
     format = Documenter.HTML(
         prettyurls = is_ci,  # Enable pretty URLs on CI/GitHub Pages
-        canonical = "https://ophelialabs.github.io/d/",  # Your documentation URL
+        canonical = "https://jlabclouds.github.io/d/",  # Your documentation URL
         # Note: omitting assets uses Documenter's default theme (CSS/JS/styling)
     ),
 )
@@ -72,9 +72,9 @@ makedocs(
 # On CI, use GITHUB_TOKEN passed through environment
 github_token = get(ENV, "GITHUB_TOKEN", nothing)
 repo_url = if is_ci && github_token !== nothing
-    "https://$(github_token)@github.com/ophelialabs/d.git"
+    "https://$(github_token)@github.com/jlabclouds/d.git"
 else
-    "github.com/ophelialabs/d.git"
+    "github.com/jlabclouds/d.git"
 end
 
 deploydocs(
